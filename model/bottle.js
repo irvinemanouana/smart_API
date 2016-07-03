270fef2e42edadfed993746fc2814463caf062a2
Manouana <christopher.manouana.sio@gmail.com>
2016-07-03 14:37:34 +0200

    bottle schema update

--- model/bottle.js
+++ model/bottle.js
@@ -6,7 +6,7 @@
 module.exports = function (app) {
     var bottleSchema = app.mongoose.Schema(
         {
-            owner :{type:String, ref:'User',required:true},
+            owner :{type:String, ref:'User'},
             date:{type:Date, default:Date.now}
         }
     );
